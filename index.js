ef7700b55d273b24c11a38d2daf4ec7ce721465f
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-10-19 09:38:57 -0300

    Assets e Finalização

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const app = express()
 const port = 3000
 
 const indexRouter = require('./routes/index')
+const tabuadaRouter = require('./routes/tabuada')
+
+app.use(express.static('public'))
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use('/', indexRouter)
+app.use('/tabuadas', tabuadaRouter)
 
 app.listen(port, () => {
   console.log('Tabuada server on port: ' + port)
